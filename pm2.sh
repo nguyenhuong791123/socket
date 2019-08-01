c12e48a2e057ff1a54d9c07bc7e72ecbe2e5cb01
nguyenhuong791123 <nguyenhuong791123@gmail.com>
2019-08-01 14:50:17 +0000

    first commit

--- pm2.sh
+++ pm2.sh
@@ -1,8 +1,33 @@
-#!/bin/bash
+#! /bin/bash
+RUN=$1
+PORT=0
 
-PORT=$1
-if [[ "$PORT" == "^[0-9]+$" ]]; then
-  NODE_PORT=${PORT} pm2 start socket.js --name socker.io-redis-${PORT}
+#### 引数の長さが 0 でなければ、
+#### PARAM_1 にそれをセット
+if [ ! -z $2 ]; then
+    PORT=$2
+fi
+#### PARAM_1 に + 1 をし、その戻り値を RET に保存
+expr $PORT + 1 > /dev/null 2>&1
+RET=$?
+#### 戻り値をもとに数字が正しいか判定
+echo "戻り値 : $RET"
+#### 戻り値を使って正常か異常か判定
+if [ $RET -lt 2 ]; then
+    echo "$PORT : 数字です"
 else
-  echo "Please setting NODE_PORT !!!"
+    echo "$PORT : 数字ではありません"
+  exit 0
+fi
+
+if [ $RUN == 'stop' -o $RUN == 'status' -o $RUN == 'delete' ]; then
+  pm2 $RUN socker.io-redis-${PORT}
+fi
+
+if [ $RUN == 'start' ]; then
+  NODE_PORT=${PORT} pm2 start socket.js --name socker.io-redis-${PORT}
+fi
+
+if [ $RUN == 'restart' ]; then
+  pm2 $RUN socker.io-redis-${PORT}
 fi
--- socket.js
+++ socket.js
@@ -3,20 +3,23 @@ const io = require('socket.io')();
 const sticky = require('sticky-session');
 const http = require('http');
 const redis = require('socket.io-redis');
+const json = require('./setting.json');
 
 const server = http.createServer((req, res) => {
   res.end('worker: ' + cluster.worker.id);
 });
 
-io.adapter(redis({ host: '192.168.10.6', port: 6379, auth_pass: "redis080" }));
+//io.adapter(redis({ host: '192.168.56.53', port: 6379 }));
+//io.adapter(redis({ host: json.redis.host, port: json.redis.port, auth_pass: json.redis.pw }));
+io.adapter(redis({ host: json.redis.host, port: json.redis.port }));
 io.attach(server);
-const NODE_PORT = process.env.NODE_PORT;
-//isWorker = sticky.listen(server, 3000);
+const NODE_PORT = process.env.NODE_PORT  || 3000;
 isWorker = sticky.listen(server, NODE_PORT);
 
 if (isWorker) {
   io.on('connection', (socket) => {
     console.log(`worker: ${cluster.worker.id}, connected, id: ${socket.id}`);
+    //socket.broadcast.emit('hello', 'to all clients except sender');
 
     socket.on('chat message', (user, message) => {
       data = `${message} from ${user}`;
